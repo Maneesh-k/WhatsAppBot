caf40ecf0eb1a238c14fe624d7e9543e9567784e
maneesh <maneeshkannan1999@gmail.com>
2022-06-20 08:50:13 +0530

    file fomate

--- routes/router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const sendMessage = require("./../app/message/message_Controller")
-const router = express.Router({ mergeParams: true });
-
-router.get("/sendmessage",sendMessage.sendMessage)
-module.exports = router;
